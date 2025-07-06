568bbe7b1848e188b17b6ff55c115964b97c9b71
Pett97 <henriquepeterson138@gmail.com>
2025-07-06 02:07:48 -0300

    ajustado_para_atualizar_descricao_compra_ajustado_background_color_card_venda

--- pix-na-mao/src/screen/order/detail-order/DetailOrderScreen.tsx
+++ pix-na-mao/src/screen/order/detail-order/DetailOrderScreen.tsx
@@ -1,25 +1,17 @@
-import { useFocusEffect, useLocalSearchParams, useRouter } from "expo-router";
-import React, { useCallback, useState } from "react";
-import { Alert, View } from "react-native";
-import { Text } from "react-native-paper";
-import Calender from "../../../components/Calender/Calender";
-import MyButton from "../../../components/MyButton/MyButton";
-import { Picker } from "@react-native-picker/picker";
-import MyInput from "../../../components/MyInput/MyInput";
-import MyPicker from "../../../components/MyPicker/MyPicker";
-import {
-  ChavePixDatabase,
-  useChavePixDatabse,
-} from "../../../database/useChavesPixDatabase";
-import {
-  ClientDatabase,
-  useClientsDatabase,
-} from "../../../database/useClientsDatabase";
-import {
-  ComprasDatabase,
-  ComprasDatabaseFormatada,
-  useOrderDatabase,
-} from "../../../database/useOrderDatabase";
+import { Picker } from '@react-native-picker/picker';
+import {useLocalSearchParams, useRouter } from 'expo-router';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Alert, View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+import Calender from '../../../components/Calender/Calender';
+import MyButton from '../../../components/MyButton/MyButton';
+import MyInput from '../../../components/MyInput/MyInput';
+import MyPicker from '../../../components/MyPicker/MyPicker';
+import { ChavePixDatabase, useChavePixDatabse } from '../../../database/useChavesPixDatabase';
+import { ClientDatabase, useClientsDatabase } from '../../../database/useClientsDatabase';
+import { ComprasDatabase, ComprasDatabaseFormatada, useOrderDatabase } from '../../../database/useOrderDatabase';
+import styleDetailOrder from './DetailOrderScreenStyle';
 
 function DetailOrderScreen() {
   const router = useRouter();
@@ -36,6 +28,7 @@ function DetailOrderScreen() {
   const [statusPagamento, setStatusPagamento] = useState<number>(0);
   const [valorVenda, setValorVenda] = useState<string>("");
   const [dataAgendamentoPix, setDataAgendamentoPix] = useState<string>("");
+  const [descricaoCompra, setDescricaoCompra] = useState<string|undefined>("");
 
   const buscarClientes = useCallback(async () => {
     try {
@@ -58,7 +51,7 @@ function DetailOrderScreen() {
   const buscarDadosCompra = useCallback(async () => {
     try {
       const compra: ComprasDatabaseFormatada | null = await DB.getCompraById(
-        Number(id),
+        Number(id)
       );
       if (compra) {
         setSelectedCliente(String(compra.idCliente));
@@ -66,10 +59,11 @@ function DetailOrderScreen() {
         setValorVenda(String(compra.valor));
         setPixAgendado(Boolean(compra.agendado));
         setStatusPagamento(compra.status);
+        setDescricaoCompra(compra.descricao);
         setDataAgendamentoPix(
           compra.dataAgendamento
             ? new Date(compra.dataAgendamento).toISOString().split("T")[0]
-            : "",
+            : ""
         );
       }
     } catch (error) {
@@ -77,16 +71,14 @@ function DetailOrderScreen() {
     }
   }, [DB, id]);
 
-  useFocusEffect(
-    useCallback(() => {
-      const carregarDados = async () => {
-        await buscarClientes();
-        await buscarChavePix();
-        await buscarDadosCompra();
-      };
-      carregarDados();
-    }, [buscarClientes, buscarChavePix, buscarDadosCompra]),
-  );
+useEffect(() => {
+  const carregarDados = async () => {
+    await buscarClientes();
+    await buscarChavePix();
+    await buscarDadosCompra();
+  };
+  carregarDados();
+}, [id]);
 
   const listaDeClientes = clientes.map((cliente) => ({
     label: cliente.nome,
@@ -107,6 +99,7 @@ function DetailOrderScreen() {
       agendado: pixAgendado ? 1 : 0,
       dataAgendamento: dataAgendamentoPix || null,
       status: statusPagamento,
+      descricao: descricaoCompra
     };
 
     try {
@@ -147,6 +140,15 @@ function DetailOrderScreen() {
           value={valorVenda}
           onChangeText={setValorVenda}
         />
+      </View>
+      <View>
+        <MyInput
+          label="Descricao Compra"
+          placeholder="ex: 3 espetinhos"
+          keyboardType="default"
+          value={descricaoCompra}
+          onChangeText={setDescricaoCompra}
+        ></MyInput>
       </View>
       <View>
         <Text>Esse pix é agendado?</Text>
@@ -192,7 +194,7 @@ function DetailOrderScreen() {
           }}
         />
       </View>
-      <View>
+      <View style={styleDetailOrder.space}>
         <MyButton title="Atualizar Venda" icon="send" action={atualizarOrder} />
       </View>
     </View>
